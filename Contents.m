152939c98b04f67a2f2cac2c3e0af2c733705dc2
Jaromir Benes <jaromir.benes@gmail.com>
2018-02-19 10:22:23 +0100

    Add README.md. Clean up.

--- Contents.m
+++ Contents.m
@@ -1,6 +1,6 @@
 % IRIS Tutorial: Unit Root Models
 %
-% Int this tutorial, we show how to build and handle models with unit-root
+% In this tutorial, we show how to build and handle models with unit-root
 % variables, such as balanced-growth path models. In IRIS, unit-root models
 % do not need to be stationarized, and can be built, simulated and analyzed
 % with the variables in their original levels.
---
+++ README.md
@@ -0,0 +1,9 @@
+## IRIS Tutorial: Unit Root Models
+
+In this tutorial, we show how to build and handle models with unit-root
+variables, such as balanced-growth path models. In IRIS, unit-root models
+do not need to be stationarized, and can be built, simulated and analyzed
+with the variables in their original levels.
+
+Start in `Contents.m`.
+
